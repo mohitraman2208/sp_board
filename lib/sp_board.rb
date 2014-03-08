6d0a466532505264f489168dc93222e35b81e717
amogh.kulkarni <amogh.kulkarni@flipkart.com>
2014-03-08 11:44:28 +0530

    fixed activerecord error

--- lib/sp_board.rb
+++ lib/sp_board.rb
@@ -1,8 +1,10 @@
 require "sp_board/version"
 require "sp_board/board"
 require "sp_board/course"
-# require "activerecord"
-Dir["sp_board/*.rb"].each {|file| require file }
+require "sp_board/grade.rb"
+require "sp_board/subject.rb"
+require "active_record"
+# Dir["sp_board/*.rb"].each {|file| require file }
 
 module SpBoard
   # Your code goes here...
